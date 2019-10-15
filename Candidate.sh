57e3786745877a913c41607d9c545d8630476235
Yuting Zhang <yzhang49@nd.edu>
2019-10-15 00:12:48 -0400

    Create a shell script named Candidate.sh

---
+++ Candidate.sh
@@ -0,0 +1,12 @@
+# Usage: bash
+
+#sort out the candidates: the presence of the methyl-coenzyme M reductase (McrA) gene& more copies of HSP70 gene
+
+#Your script should include all of the necessary steps to search each genome for the genes of interest and should produce a summary table collating the results of all searches. 
+
+#1) use Muscle to make alignment of those genes
+#2)hmmbuild: align in hmm out;
+#3)hmmsearch: find and match
+
+
+
